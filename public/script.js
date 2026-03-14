da9f3e2f26ccdc55813ceb0e0fe50afda43c5e7e
kaio69234-rgb <kaio69234@gmail.com>
2026-03-14 16:57:30 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,23 @@
-function iniciarBot(){
-fetch("/start")
-.then(res=>res.text())
-.then(msg=>{
-alert(msg)
-})
+async function uploadBot(){
+
+const fileInput = document.getElementById("botFile")
+
+if(!fileInput.files.length){
+alert("Envie um bot .zip")
+return
 }
 
-function pararBot(){
-fetch("/stop")
-.then(res=>res.text())
-.then(msg=>{
-alert(msg)
+const form = new FormData()
+
+form.append("bot", fileInput.files[0])
+
+const res = await fetch("/upload",{
+method:"POST",
+body:form
 })
+
+const data = await res.json()
+
+alert(data.status)
+
 }
